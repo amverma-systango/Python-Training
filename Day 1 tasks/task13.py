1f5457340eb5bd7d0b647b74dd1ff92e1e65f11e
amverma-systango <amverma@isystango.com>
2022-04-20 18:56:28 +0530

    task 13 added

---
+++ Day 1 tasks/task13.py
@@ -0,0 +1,10 @@
+# 14) Function Argument
+
+
+def fun( num1, num2): #formal argument
+	print("now this is called a function")
+	print("it take 2 arguments. Here the are")
+	print(num1, num2)
+
+
+fun(564, 789) #actual argument
