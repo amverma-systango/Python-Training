84f651c3feb83c4581db2ae96e324d380709e511
amverma-systango <amverma@isystango.com>
2022-04-28 20:35:46 +0530

    made user signup using class based view

--- Day 5 task/myFirstPage/polls/urls.py
+++ Day 5 task/myFirstPage/polls/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Index, name="Index")
+    path('', views.Index.as_view(), name="Index"),
+    path('login', views.Login.as_view(), name="Login"),
+    path('signup', views.Signup.as_view(), name="Signup"),
 ]
--- Day 5 task/myFirstPage/polls/views.py
+++ Day 5 task/myFirstPage/polls/views.py
@@ -1,7 +1,62 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import View, TemplateView, CreateView
+from django.contrib.auth.views import LoginView
+
+from .models import User, Question, Choice, Participant
 
 # Create your views here.
 
-def Index(request):
-    return HttpResponse("I am in Polls app")
+class Index(TemplateView):
+    http_method_names = ['get']
+    template_name = 'polls/index.html'
+    
+    
+
+
+
+
+class Login(LoginView):
+    http_method_names = ['get', 'post']
+    template_name = "polls/login.html"
+    # success_url = 'signup'
+    
+    
+
+    # def post(self, request):
+    #     pass
+
+
+
+
+
+class Signup(TemplateView):
+    http_method_names = ['get', 'post']
+    template_name = "polls/signup.html"
+    
+    
+    def post(self, request):
+        
+        # try:
+        #     user = User.objects.filter()
+    
+        # except:
+        #     pass
+
+        userNameInp = request.POST['nameInp']
+        userPasswordInp = request.POST['passwordInp']
+
+        print(userNameInp, userPasswordInp)
+
+        user = User.objects.filter( user_name = userNameInp )
+
+        print(user)
+
+    
+        if( len(user) == 0 ):
+            createUser = User( user_name = userNameInp, user_password = userPasswordInp)
+            createUser.save()
+        else:
+            return HttpResponse("username unavailable please try again")
+
+
